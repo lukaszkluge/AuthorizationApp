8c7fa1f96e3fc9cd69a287435128afd369b11b2a
Łukasz Kluge <kluge.lukasz@gmail.com>
2023-10-17 15:19:07 +0200

    Check Authorization

--- AuthorizationApp/Services/AuthController.cs
+++ AuthorizationApp/Services/AuthController.cs
@@ -6,27 +6,49 @@ using System.Threading.Tasks;
 
 namespace AuthorizationApp.Services
 {
-    public class AuthController()
+    public class AuthController
     {
-        _authService = new AuthService();     // authorization service initialization
+        private readonly AuthService _authService;
+        public AuthController()
+        {
+            _authService = new AuthService();
+        }
+
+        public void RegisterAndAuthenticate()
+        {
+            // User registration and authorization simulation
+            Console.Write("Enter your username:");
+            var username = Console.ReadLine();
+
+            Console.Write("Enter your password:");
+            var password = Console.ReadLine();
+
+            // user registration 
+            var token = _authService.RegisterUser(username, password);
+
+            Console.WriteLine("User registered and JWT generated:");
+            Console.WriteLine(token);
+
+            // In a real project, the token should be saved and passed to the API in headers
+        }
+
+        public void CheckAuthorization()
+        {
+            Console.WriteLine("Enter JWT token:");
+            var token = Console.ReadLine();
+
+            bool isAuthenticated = _authService.AuthenticateUser(token);
+
+            if (isAuthenticated)
+            {
+                Console.WriteLine("User is authorized.");
+            }
+            else
+            {
+                Console.WriteLine("User is not authorized.");
+            }
+        }
+
     }
-    
-    public void RegisterAndAuthenticate()
-    {
-        // User registration and authorization simulation
-        Console.Write("Enter your username:");
-        var username = Console.ReadLine();
-
-        Console.Write("Enter your password:");
-        var password = Console.ReadLine();
-
-        // user registration 
-        var token = _authService.RegisterUser(username, password);
-
-        Console.WriteLine("User registered and JWT generated:");
-        Console.WriteLine(token);
-
-        // In a real project, the token should be saved and passed to the API in headers
-    }
-
 }
+
