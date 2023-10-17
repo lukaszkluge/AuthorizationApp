1dea8fdef2d883a5e25e9f5475116535618253a2
Łukasz Kluge <kluge.lukasz@gmail.com>
2023-10-17 11:57:06 +0200

    Register and Authenticate function

--- AuthorizationApp/Services/AuthController.cs
+++ AuthorizationApp/Services/AuthController.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace AuthorizationApp.Services
 {
-    internal class AuthController
+    public class AuthController()
     {
+        _authService = new AuthService();     // authorization service initialization
     }
+    
+    public void RegisterAndAuthenticate()
+    {
+        // User registration and authorization simulation
+        Console.Write("Enter your username:");
+        var username = Console.ReadLine();
+
+        Console.Write("Enter your password:");
+        var password = Console.ReadLine();
+
+        // user registration 
+        var token = _authService.RegisterUser(username, password);
+
+        Console.WriteLine("User registered and JWT generated:");
+        Console.WriteLine(token);
+
+        // In a real project, the token should be saved and passed to the API in headers
+    }
+
 }
